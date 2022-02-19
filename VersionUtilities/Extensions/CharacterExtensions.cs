6467eaf99fe1b9c3ff9f101f351ca34dc7733f1b
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-19 02:27:31 -0500

    move character extensions into its own namespace

---
+++ VersionUtilities/Extensions/CharacterExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AssetRipper.VersionUtilities.Extensions
+{
+	/// <summary>
+	/// Extensions for <see cref="char"/>
+	/// </summary>
+	public static class CharacterExtensions
+	{
+		internal static int ParseDigit(this char _this)
+		{
+			return _this - '0';
+		}
+
+		/// <summary>
+		/// Parse a character into a Unity Version Type
+		/// </summary>
+		/// <param name="c">The character</param>
+		/// <returns></returns>
+		/// <exception cref="ArgumentException">No version type for character</exception>
+		public static UnityVersionType ToUnityVersionType(this char c)
+		{
+			return c switch
+			{
+				'a' => UnityVersionType.Alpha,
+				'b' => UnityVersionType.Beta,
+				'c' => UnityVersionType.China,
+				'f' => UnityVersionType.Final,
+				'p' => UnityVersionType.Patch,
+				'x' => UnityVersionType.Experimental,
+				_ => throw new ArgumentException($"There is no version type {c}", nameof(c)),
+			};
+		}
+	}
+}
