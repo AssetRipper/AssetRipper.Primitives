86eeee61946e8e0b7a157a2a86ab932f8ca7005e
Jeremy Pritts <jrpritts7@gmail.com>
2023-01-15 12:22:26 -0500

    Generic math for .NET 7

---
+++ VersionUtilities/UnityVersion.DotNet7.cs
@@ -0,0 +1,11 @@
+﻿#if NET7_0_OR_GREATER
+using System.Numerics;
+
+namespace AssetRipper.VersionUtilities;
+partial struct UnityVersion : IComparisonOperators<UnityVersion, UnityVersion, bool>, IEqualityOperators<UnityVersion, UnityVersion, bool>, IMinMaxValue<UnityVersion>
+{
+	static UnityVersion IMinMaxValue<UnityVersion>.MaxValue => MaxVersion;
+
+	static UnityVersion IMinMaxValue<UnityVersion>.MinValue => MinVersion;
+}
+#endif
