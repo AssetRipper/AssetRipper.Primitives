eedce8a128ed4749c415d238c3e495a900c96080
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-08-07 10:41:28 -0400

    Utf8String concatenation

--- AssetRipper.Primitives.Tests/Utf8StringTests.cs
+++ AssetRipper.Primitives.Tests/Utf8StringTests.cs
@@ -4,6 +4,15 @@ namespace AssetRipper.Primitives.Tests;
 public class Utf8StringTests
 {
 	[Test]
+	public void Utf8StringEqualityWorks()
+	{
+		Utf8String utf8Str = new Utf8String("AssetRipperTestString");
+		Utf8String utf8Str2 = new Utf8String(utf8Str.Data.ToArray());
+
+		Assert.That(utf8Str2, Is.EqualTo(utf8Str));
+	}
+
+	[Test]
 	public void StringEmptyIsEquivalentToUtf8StringEmpty()
 	{
 		Assert.Multiple(() =>
@@ -27,6 +36,52 @@ public class Utf8StringTests
 		Utf8String dataString = "AssetRipperTestString"u8;
 		Utf8String systemString = "AssetRipperTestString";
 		Assert.That(dataString, Is.EqualTo(systemString));
+	}
+
+	[Test]
+	public void ConcatenationBetweenTwoUtf8StringsWorks()
+	{
+		Utf8String dataString = "AssetRipperTestString"u8;
+		Utf8String systemString = "AssetRipperTestString";
+		Utf8String concatenated = dataString + systemString;
+		Assert.That(concatenated, Is.EqualTo((Utf8String)"AssetRipperTestStringAssetRipperTestString"u8));
+	}
+
+	[Test]
+	public void ConcatenationBetweenThreeUtf8StringsWorksTheSameAsAddition()
+	{
+		Utf8String added = "String1"u8 + new Utf8String("2") + "3"u8;
+		Utf8String concatenated = Utf8String.Concat("String1"u8, "2"u8, "3"u8);
+		Assert.That(added, Is.EqualTo(concatenated));
+	}
+
+	[Test]
+	public void ConcatenationBetweenFourUtf8StringsWorks()
+	{
+		Utf8String string1 = "hello"u8;
+		Utf8String string2 = "world"u8;
+		Utf8String string3 = "!"u8;
+		Utf8String string4 = ", how are you?"u8;
+		Utf8String concatenated = Utf8String.Concat(string1, string2, string3, string4);
+		Assert.That(concatenated, Is.EqualTo((Utf8String)"hello world!, how are you?"u8));
+	}
+
+	[Test]
+	public void IsNullOrEmptyReturnsTrueForEmptyString()
+	{
+		Assert.That(Utf8String.IsNullOrEmpty(Utf8String.Empty), Is.True);
+	}
+
+	[Test]
+	public void IsNullOrEmptyReturnsTrueForNullString()
+	{
+		Assert.That(Utf8String.IsNullOrEmpty(null), Is.True);
+	}
+
+	[Test]
+	public void IsNullOrEmptyReturnsFalseForNonEmptyString()
+	{
+		Assert.That(Utf8String.IsNullOrEmpty("AssetRipperTestString"u8), Is.False);
 	}
 }
 #endif
--- AssetRipper.Primitives/Utf8String.cs
+++ AssetRipper.Primitives/Utf8String.cs
@@ -27,9 +27,13 @@ public sealed class Utf8String : IEquatable<Utf8String>
 		cachedString = "";
 	}
 
-	public Utf8String(ReadOnlySpan<byte> data)
+	public Utf8String(ReadOnlySpan<byte> data) : this(data.ToArray())
 	{
-		this.data = data.ToArray();
+	}
+
+	private Utf8String(byte[] data)
+	{
+		this.data = data;
 	}
 
 	public Utf8String(string @string)
@@ -100,6 +104,198 @@ public sealed class Utf8String : IEquatable<Utf8String>
 	public static implicit operator ReadOnlySpan<byte>(Utf8String? utf8String)
 	{
 		return utf8String is null ? default : utf8String.Data;
+	}
+
+	public static Utf8String operator +(Utf8String? string1, Utf8String? string2)
+	{
+		return Concat(string1, string2);
+	}
+
+	public static Utf8String operator +(Utf8String? string1, ReadOnlySpan<byte> string2)
+	{
+		return string1 is null
+			? string2.Length == 0 ? Empty : new Utf8String(string2)
+			: Concat(string1.Data, string2);
+	}
+
+	public static Utf8String operator +(ReadOnlySpan<byte> string1, Utf8String? string2)
+	{
+		return string2 is null
+			? string1.Length == 0 ? Empty : new Utf8String(string1)
+			: Concat(string1, string2.Data);
+	}
+
+	public static bool IsNullOrEmpty([NotNullWhen(false)] Utf8String? utf8String)
+	{
+		return utf8String is null || utf8String.IsEmpty;
+	}
+
+	public static Utf8String Concat(Utf8String? string1, Utf8String? string2)
+	{
+		if (IsNullOrEmpty(string1))
+		{
+			return string2 ?? Empty;
+		}
+		if (IsNullOrEmpty(string2))
+		{
+			return string1;
+		}
+
+		byte[] data = new byte[string1.data.Length + string2.data.Length];
+		string1.data.CopyTo(data.AsSpan());
+		string2.data.CopyTo(data.AsSpan(string1.data.Length));
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(Utf8String? string1, Utf8String? string2, Utf8String? string3)
+	{
+		if (IsNullOrEmpty(string1))
+		{
+			return Concat(string2, string3);
+		}
+		if (IsNullOrEmpty(string2))
+		{
+			return Concat(string1, string3);
+		}
+		if (IsNullOrEmpty(string3))
+		{
+			return Concat(string1, string2);
+		}
+
+		byte[] data = new byte[string1.data.Length + string2.data.Length + string3.data.Length];
+		string1.data.CopyTo(data.AsSpan());
+		string2.data.CopyTo(data.AsSpan(string1.data.Length));
+		string3.data.CopyTo(data.AsSpan(string1.data.Length + string2.data.Length));
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(Utf8String? string1, Utf8String? string2, Utf8String? string3, Utf8String? string4)
+	{
+		if (IsNullOrEmpty(string1))
+		{
+			return Concat(string2, string3, string4);
+		}
+		if (IsNullOrEmpty(string2))
+		{
+			return Concat(string1, string3, string4);
+		}
+		if (IsNullOrEmpty(string3))
+		{
+			return Concat(string1, string2, string4);
+		}
+		if (IsNullOrEmpty(string4))
+		{
+			return Concat(string1, string2, string3);
+		}
+
+		byte[] data = new byte[string1.data.Length + string2.data.Length + string3.data.Length + string4.data.Length];
+		string1.data.CopyTo(data.AsSpan());
+		string2.data.CopyTo(data.AsSpan(string1.data.Length));
+		string3.data.CopyTo(data.AsSpan(string1.data.Length + string2.data.Length));
+		string4.data.CopyTo(data.AsSpan(string1.data.Length + string2.data.Length + string3.data.Length));
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(params Utf8String?[] values)
+	{
+		ArgumentNullException.ThrowIfNull(values);
+
+		int totalLength = 0;
+		foreach (Utf8String? value in values)
+		{
+			if (value is null)
+			{
+				continue;
+			}
+
+			totalLength += value.data.Length;
+		}
+
+		if (totalLength == 0)
+		{
+			return Empty;
+		}
+
+		byte[] data = new byte[totalLength];
+		int offset = 0;
+		foreach (Utf8String? value in values)
+		{
+			if (value is null)
+			{
+				continue;
+			}
+
+			value.data.CopyTo(data.AsSpan(offset));
+			offset += value.data.Length;
+		}
+
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(ReadOnlySpan<byte> string1, ReadOnlySpan<byte> string2)
+	{
+		if (string1.Length == 0)
+		{
+			return string2.Length == 0 ? Empty : new Utf8String(string2);
+		}
+		if (string2.Length == 0)
+		{
+			return new Utf8String(string1);
+		}
+
+		byte[] data = new byte[string1.Length + string2.Length];
+		string1.CopyTo(data);
+		string2.CopyTo(data.AsSpan(string1.Length));
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(ReadOnlySpan<byte> string1, ReadOnlySpan<byte> string2, ReadOnlySpan<byte> string3)
+	{
+		if (string1.Length == 0)
+		{
+			return Concat(string2, string3);
+		}
+		if (string2.Length == 0)
+		{
+			return Concat(string1, string3);
+		}
+		if (string3.Length == 0)
+		{
+			return Concat(string1, string2);
+		}
+
+		byte[] data = new byte[string1.Length + string2.Length + string3.Length];
+		string1.CopyTo(data);
+		string2.CopyTo(data.AsSpan(string1.Length));
+		string3.CopyTo(data.AsSpan(string1.Length + string2.Length));
+		return new Utf8String(data);
+	}
+
+	public static Utf8String Concat(ReadOnlySpan<byte> string1, ReadOnlySpan<byte> string2, ReadOnlySpan<byte> string3, ReadOnlySpan<byte> string4)
+	{
+		if (string1.Length == 0)
+		{
+			return Concat(string2, string3, string4);
+		}
+		if (string2.Length == 0)
+		{
+			return Concat(string1, string3, string4);
+		}
+		if (string3.Length == 0)
+		{
+			return Concat(string1, string2, string4);
+		}
+		if (string4.Length == 0)
+		{
+			return Concat(string1, string2, string3);
+		}
+
+		byte[] data = new byte[string1.Length + string2.Length + string3.Length + string4.Length];
+		string1.CopyTo(data);
+		string2.CopyTo(data.AsSpan(string1.Length));
+		string3.CopyTo(data.AsSpan(string1.Length + string2.Length));
+		string4.CopyTo(data.AsSpan(string1.Length + string2.Length + string3.Length));
+		return new Utf8String(data);
 	}
 }
 #endif
