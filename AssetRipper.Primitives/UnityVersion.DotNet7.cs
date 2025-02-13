5ab5d744d6dce4fe45df5e6ff1085363aa65dc69
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-13 15:57:20 -0800

    UnityVersion : IParsable<UnityVersion>

--- AssetRipper.Primitives/UnityVersion.DotNet7.cs
+++ AssetRipper.Primitives/UnityVersion.DotNet7.cs
@@ -2,10 +2,24 @@
 using System.Numerics;
 
 namespace AssetRipper.Primitives;
-partial struct UnityVersion : IComparisonOperators<UnityVersion, UnityVersion, bool>, IEqualityOperators<UnityVersion, UnityVersion, bool>, IMinMaxValue<UnityVersion>
+partial struct UnityVersion :
+	IComparisonOperators<UnityVersion, UnityVersion, bool>,
+	IEqualityOperators<UnityVersion, UnityVersion, bool>,
+	IMinMaxValue<UnityVersion>,
+	IParsable<UnityVersion>
 {
 	static UnityVersion IMinMaxValue<UnityVersion>.MaxValue => MaxVersion;
 
 	static UnityVersion IMinMaxValue<UnityVersion>.MinValue => MinVersion;
+
+	static UnityVersion IParsable<UnityVersion>.Parse(string s, IFormatProvider? provider)
+	{
+		return Parse(s);
+	}
+
+	static bool IParsable<UnityVersion>.TryParse(string? s, IFormatProvider? provider, out UnityVersion result)
+	{
+		return TryParse(s, out result, out _);
+	}
 }
 #endif
