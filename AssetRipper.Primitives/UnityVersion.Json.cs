cd7f84184502abbe403585123f0f5f8e128fb436
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-13 15:56:24 -0800

    Json serialization support for UnityVersion

---
+++ AssetRipper.Primitives/UnityVersion.Json.cs
@@ -0,0 +1,10 @@
+﻿#if NET5_0_OR_GREATER
+using System.Text.Json.Serialization;
+
+namespace AssetRipper.Primitives;
+
+[JsonConverter(typeof(UnityVersionJsonConverter))]
+partial struct UnityVersion
+{
+}
+#endif
---
+++ AssetRipper.Primitives/UnityVersionJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿#if NET5_0_OR_GREATER
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AssetRipper.Primitives;
+
+public sealed class UnityVersionJsonConverter : JsonConverter<UnityVersion>
+{
+	public override UnityVersion Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	{
+		return UnityVersion.Parse(reader.GetString() ?? throw new JsonException("String was read as null"));
+	}
+
+	public override void Write(Utf8JsonWriter writer, UnityVersion value, JsonSerializerOptions options)
+	{
+		writer.WriteStringValue(value.ToString());
+	}
+}
+#endif
